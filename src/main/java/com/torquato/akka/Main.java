4e184153c55a0c125c3964a0cb789753777b6ba5
Rafael Torquato <rafael.torquato@lbty.com.br>
2024-09-16 20:38:01 -0300

    WIP

---
+++ src/main/java/com/torquato/akka/Main.java
@@ -0,0 +1,7 @@
+package com.torquato.akka;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ src/main/java/com/torquato/akka/bigprimes/BigPrimesMain.java
@@ -0,0 +1,55 @@
+package com.torquato.akka.bigprimes;
+
+import akka.Done;
+import akka.NotUsed;
+import akka.actor.typed.ActorSystem;
+import akka.actor.typed.javadsl.Behaviors;
+import akka.stream.javadsl.Flow;
+import akka.stream.javadsl.RunnableGraph;
+import akka.stream.javadsl.Sink;
+import akka.stream.javadsl.Source;
+import lombok.extern.slf4j.Slf4j;
+
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.CompletionStage;
+
+@Slf4j
+public class BigPrimesMain {
+
+    public static void main(String[] args) {
+        final Random random = new Random();
+        final Source<Integer, NotUsed> source = Source.range(0, 9);
+
+        final Flow<Integer, BigInteger, NotUsed> convertToBigInteger = Flow.of(Integer.class)
+                .map(i -> new BigInteger(2000, new Random()));
+
+        final Flow<BigInteger, BigInteger, NotUsed> generateProbablePrime = Flow.of(BigInteger.class)
+                .map(BigInteger::nextProbablePrime);
+
+        final Flow<BigInteger, List<BigInteger>, NotUsed> groupAndSort = Flow.of(BigInteger.class)
+                .grouped(10)
+                .map(v -> {
+                    final ArrayList<BigInteger> copy = new ArrayList<>(v);
+                    copy.sort(Comparator.naturalOrder());
+                    return copy;
+                });
+
+
+        final Sink<List<BigInteger>, CompletionStage<Done>> printSink = Sink.foreach(v -> log.info("{}", v));
+
+        final RunnableGraph<NotUsed> runnableGraph = source
+                .via(convertToBigInteger)
+                .via(generateProbablePrime)
+                .via(groupAndSort)
+                .to(printSink);
+
+        final ActorSystem<Object> actorSystem = ActorSystem.create(Behaviors.empty(), "BigPrimes");
+
+        runnableGraph.run(actorSystem);
+    }
+
+}
